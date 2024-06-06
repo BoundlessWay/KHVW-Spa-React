e432dd6c331ac4cd6e5b63bab22f5d58de66cf4c
BoundlessWay <70361753+BoundlessWay@users.noreply.github.com>
2024-06-07 06:06:45 +0700

    fix warning babel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Sidebar from './components/Sidebar/Sidebar';
 import MainContent from './components/MainContent/MainContent';
